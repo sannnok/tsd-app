1b0a363507cc4b478d078deb5feb9375b78164ed
Alex R. <sannnok@yandex.ru>
2025-03-02 11:20:34 +0200

    Ititial commit - define starter pack

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import styled from 'styled-components';
+import { StyledEngineProvider } from '@mui/material';
+
+const BaseButton = styled(Button)`
+  padding: 10px 20px;
+  border-radius: 5px;
+`;
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <BaseButton onClick={() => setCount((count) => count + 1)} variant="outlined">count is {count}</BaseButton>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </StyledEngineProvider>
+  )
+}
+
+export default App
